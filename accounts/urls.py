2b935b553d5638fc5aa75966027225c667f4793a
Ritik Raj <ritikraj2699@outlook.com>
2023-07-07 13:54:48 +0530

    added email authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import login, signup
+from accounts.views import login, signup, activate_email
 
 urlpatterns = [
     path('login/',login, name="login"),
-    path('signup/',signup, name="signup")
+    path('signup/',signup, name="signup"),
+    path('activate/<email_token>/',activate_email, name="activate_email")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Profile
+
 
 def login(request):
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(username=email)
+
+        if not user_obj.exists():
+            messages.warning(request, 'Account does not exist')
+            return HttpResponseRedirect(request.path_info)
+        
+        if not user_obj[0].profile.is_email_verified:
+            messages.warning(request, 'Your account is not verified.')
+            return HttpResponseRedirect(request.path_info)
+
+        user_obj.authenticate(username=email,password=password)
+
+        if user_obj:
+            login(request,user_obj)
+            return redirect('/')
+
+        messages.warning(request,'Invalid credentials')
+        return HttpResponseRedirect(request.path_info)
+    
     return render(request,'accounts/login.html')
 
 
@@ -14,13 +40,14 @@ def signup(request):
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
-
         user_obj = User.objects.filter(username=email)
 
         if user_obj.exists():
             messages.warning(request, 'Email already taken')
             return HttpResponseRedirect(request.path_info)
         
+        print(email)
+
         user_obj = User.objects.create(first_name=first_name,last_name=last_name,email=email,username=email)
         user_obj.set_password(password)
         user_obj.save()
@@ -29,3 +56,13 @@ def signup(request):
         return HttpResponseRedirect(request.path_info)
     
     return render(request,'accounts/signup.html')
+
+
+def activate_email(request, email_token):
+    try:
+        user_profile = Profile.objects.get(email_token=email_token)
+        user_profile.is_email_verified = True
+        user_profile.save()
+        return redirect('/')
+    except Exception as e:
+        return HttpResponse('Invalid email token')
