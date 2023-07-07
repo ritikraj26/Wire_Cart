b30e9377a13f661e389436c911cc4a1a74e89ca2
Ritik Raj <ritikraj2699@outlook.com>
2023-07-07 22:06:17 +0530

    populated the products section and added product details

--- wire_cart/urls.py
+++ wire_cart/urls.py
@@ -21,8 +21,10 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('',include('home.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('product/', include('products.urls'))
 ]
 
 if settings.DEBUG:
