7e25750f1a14342c7f4b7cb18c51b2bbfb481ee9
Ritik Raj <ritikraj2699@outlook.com>
2023-07-08 02:29:58 +0530

    added size variants

--- products/views.py
+++ products/views.py
@@ -5,7 +5,16 @@ from products.models import Product
 def get_product(request, slug):
     try:
         product = Product.objects.get(slug=slug)
-        return render(request,'product/product.html',context = {'product':product})
+        context = {'product':product}
+
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            price = product.get_product_price_by_size(size)
+            context['selected_size'] = size
+            context['updated_price'] = price
+
+        return render(request,'product/product.html',context)
+    
     except Exception as e:
         print(e)
 
